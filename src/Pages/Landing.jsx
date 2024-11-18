0c7f1d9c6c480bc3fd1c7ed4bf6588960bffdcd4
Samson Iweibo <samsoniweibo@gmail.com>
2024-11-18 17:41:48 +0100

    1st commit

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//icon(s)
+import { FaCircleChevronRight } from "react-icons/fa6";
+
+const Landing = () => {
+  return (
+    <div className="pt-[81px] px-[26px]">
+      <div className="">
+        <img
+          className="w-[342px] h-[313px]"
+          src="/vecteezy_ai-generated-close-up-of-cute-cartoon-jesus-christ-icon_42353353 1.png"
+          alt="jesus-icon"
+        />
+        <p className="font-Inria font-bold text-[36px] text-customGold text-center py-8">
+          Christville
+        </p>
+      </div>
+      <p className="font-Poppins text-[18px] text-center">
+        Welcome to Faith, the ultimate Bible token designed to enrich your
+        spiritual journey
+      </p>
+      <button className="text-[55px] py-[50px] relative left-[40%]">
+        <FaCircleChevronRight className="text-customGold" />
+      </button>
+      <div className="pt-[0px] flex flex-col items-center justify-center">
+        <p className="font-Poppins pb-[17px]">Follow us on social media</p>
+        <section className="flex gap-5">
+          <Link to="#">
+            <img src="/instagram.png" alt="instagram" />
+          </Link>
+          <Link to="#">
+            <img src="/youtube.png" alt="youtube" />
+          </Link>
+          <Link to="#">
+            <img src="/twitter.png" alt="x" />
+          </Link>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inria: ['"Inria Serif"', "serif"],
+        Poppins: ['"Poppins"', "sans-serif"],
+      },
+      colors: {
+        customGold: "#DDBF5F"
+      }
+    },
   },
   plugins: [],
 }
