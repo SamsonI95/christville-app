768d75ec68ab3950e26f36044e71a4861e0eebb6
Samson Iweibo <samsoniweibo@gmail.com>
2025-01-28 09:44:48 +0100

    Updating Telegram user Image - phase 2

--- src/Pages/AppPages/LeaderBoard.jsx
+++ src/Pages/AppPages/LeaderBoard.jsx
@@ -2,7 +2,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../../Usercontext";
-import { getTelegramUser } from "./telegramUtils";
+import { getTelegramUser } from "../../Components/telegramUtils";
 
 //Icon(s)
 import { IoMdInformationCircleOutline } from "react-icons/io";
